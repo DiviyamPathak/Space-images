37a761bcc6d9bbe5e644b729ea714c21faf76dd5
DiviyamPathak <patdiviyam@gmail.comm>
2021-06-27 05:08:04 +0530

    array link problem

--- nasaimages.js
+++ nasaimages.js
@@ -6,9 +6,35 @@ let inputval = input.nodeValue
 function searchfunction(_inputval){
     return fetch("https://images-api.nasa.gov/search?description={moon}")
             .then((res) => res.json())
-            .then(data => {const images = data
-                console.log(images) })
-            .catch()
+            .then(data => {
+                    const images = data
+                    //const descriptorlin = Object.getOwnPropertyDescriptor(images, "collection")
+                    //const arrres = Object.values(descriptorlin)
+                    //const iop = arrres[0]
+                    //const arrayofitems = Object.getOwnPropertyDescriptor(iop, "items")
+                    //console.log(arrayofitems.value)
+                    const arrayoflinks = Object.values(images.collection.items)
+                    let imgarray
+                    for (i in arrayoflinks)
+                    {
+                        let imgtag = document.createElement("img")
+                        let imgsrc = document.createAttribute("src")
+                        imgarray = arrayoflinks[i]
+                        imgsrc.value = (imgarray.href)
+                        imgtag.setAttributeNode(imgsrc)
+                        document.body.appendChild(imgtag)
+                    }
+                    console.log(arrayoflinks)
+                    
+                        fetch(imgarray.href)
+                            .then(console.log(data)
+                            )})
+            
+                            
+            
+            
+
+
 }
 
 button.addEventListener("click", searchfunction)
