0b5d020fa49eb5e68428e4d0de52351d534c0c71
DiviyamPathak <patdiviyam@gmail.comm>
2021-06-27 19:25:37 +0530

    working but not with results not found features

--- nasaimages.js
+++ nasaimages.js
@@ -1,34 +1,39 @@
-let input = document.getElementById("inputi");
+
 let button = document.getElementById("search");
 
-let inputval = input.nodeValue
+
 
 function searchfunction(_inputval){
-    return fetch("https://images-api.nasa.gov/search?description={moon}")
-            .then((res) => res.json())
-            .then(data => {
-                    const images = data
-                    //const descriptorlin = Object.getOwnPropertyDescriptor(images, "collection")
-                    //const arrres = Object.values(descriptorlin)
-                    //const iop = arrres[0]
-                    //const arrayofitems = Object.getOwnPropertyDescriptor(iop, "items")
-                    //console.log(arrayofitems.value)
-                    const arrayoflinks = Object.values(images.collection.items)
-                    let imgarray
-                    for (i in arrayoflinks)
-                    {
+    let input = document.getElementById("inputi")
+    let inputval = input.value
+    let url = "https://images-api.nasa.gov/search?description=" + "{" + inputval + "}"
+    fetch(url)
+        .then((res) => res.json())
+        .then
+            (data => {
+                 const images = data
+                 const arrayoflinks = Object.values(images.collection.items)
+                let imgarray
+                for (i in arrayoflinks)
+                {
                         let imgtag = document.createElement("img")
                         let imgsrc = document.createAttribute("src")
                         imgarray = arrayoflinks[i]
-                        imgsrc.value = (imgarray.href)
+                        imgsrc.value = imgarray.links[0].href
                         imgtag.setAttributeNode(imgsrc)
                         document.body.appendChild(imgtag)
                     }
-                    console.log(arrayoflinks)
+                
                     
-                        fetch(imgarray.href)
-                            .then(console.log(data)
-                            )})
+                        
+                            
+                
+                }
+                
+                
+            )
+
+    
             
                             
             
